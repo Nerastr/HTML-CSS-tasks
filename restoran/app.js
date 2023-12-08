7bace3a89a06af6b1f8548f34f50ae66c6cf8369
Nerastr <106918395+Nerastr@users.noreply.github.com>
2023-12-08 17:53:20 +0300

    Delete restoran directory

--- restoran/app.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $('#header-button').click(function(){
-    $('html, body').animate({scrollTop: $(document).height() - $(window).height()}, 600);
-    return false;
-    });
-  });
